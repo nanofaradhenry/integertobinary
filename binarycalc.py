17cd2f00d696c7b1d47ed735d69fc146025e08b8
nanofaradhenry <90361088+nanofaradhenry@users.noreply.github.com>
2021-09-09 06:01:37 -0700

    Add files via upload

---
+++ binarycalc.py
@@ -0,0 +1,11 @@
+import time
+from typing import Type
+print("Welcome to binary converter")
+
+new_input=int(input("Enter a number to receive its binary equivalent:"))
+print(bin(new_input))
+time.sleep(2)
+print("0b indicates that the following number is binary ")
+time.sleep(0)
+print(type(new_input))
+
